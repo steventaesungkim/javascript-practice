966cfcb0aa9dcf07674c09648e60d76365f42404
Steven Taesung Kim <steventaesungkim@gmail.com>
2018-10-04 15:22:52 -0400

    going over name

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,61 @@
 // This is javascript
 
+// console.log("Hello Javascript")
+
+
+// hello.py
+// getting input
+// it's lame.
+
+// using var
+// and camelCase
+// var promptText = "What is your name";
+// var userName = prompt(promptText);
+
+// // string concatentation
+// var greeting = "Hello, " + userName + "!";
+// console.log(greeting);
+
+// now using sexy syntax
+// using let and const
+// rule of thumb: use const until you get an err
+
+// const prompt_Text = "What is your name?";
+// // let userName = prompt(prompt_Text);
+// let userName = "steven";
+// userName = userName + " is a bozo";
+// // const greeting = "Hello, " + userName + "!";
+// // console.log(greeting);
+
+
+
+// // string interpolation
+// // 1. Replace quotes with backticks "`"
+// // 2. remove the + signs
+// // 3. wrap your var with ${}
+
+
+// const greeting = `Hello, ${userName}!`;
+// console.log(greeting);
+
+
+
+//  moving onto madlibs
+
+// Hard-coding instead of prompting because in real life,
+// when you want user input, you'll create an HTML form.
+
+// using backticks for hipster strings
+const name = `chris aquino1`;
+const subject = `feline husbandry`;
+
+const finalText = `${name}'s favorite subject in school is ${subject}.
+
+
+I can do line breaks.
+`;
+// or, you can use \n for line breaks
+// const finalText = `${name}'s favorite subject in school is ${subject}. \n\nThis is the line break.;
+
+console.log(finalText);
+
